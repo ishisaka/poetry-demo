257ecc719ff299ddfc7a3ba5e80684ff44e9b0ca
Tadahiro Ishisaka <tishisaka@baleen.studio>
2024-07-17 13:49:02 +0900

    numpy追加

--- poetry_demo/main.py
+++ poetry_demo/main.py
@@ -1,8 +1,14 @@
 import sys
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def main() -> int:
+    """メイン関数
+
+    Returns:
+        int: 正常終了時は0を返す
+    """
     # 整数型の配列を用意
     arr_int32 = np.array([100, 200, 300, 400, 500], dtype=np.int32)
     print(arr_int32)
@@ -14,6 +20,19 @@ def main() -> int:
     # 行列計算
     arr_sum = arr_int32 + arr_float
     print(arr_sum)
+
+    # データ作成
+    x = np.linspace(0, 10, 100)
+    y = x + np.random.randn(100)
+
+    # プロット
+    plt.plot(x, y, label="Test")
+
+    # 凡例
+    plt.legend()
+
+    # 表示
+    plt.show()
 
     return 0
 
